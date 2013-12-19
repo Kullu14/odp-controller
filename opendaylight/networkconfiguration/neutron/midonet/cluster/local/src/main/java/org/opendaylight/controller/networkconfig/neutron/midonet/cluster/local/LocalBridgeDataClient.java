639049d6ec6763e123cf4a5e95d16f8ad8bf70ab
Tomohiko KIMURA <tomohiko@midokura.com>
2013-12-19 12:34:07 +0900

    Added call from LocalBridgeDataClient to the DataClient stub.

--- opendaylight/networkconfiguration/neutron/midonet/cluster/local/src/main/java/org/opendaylight/controller/networkconfig/neutron/midonet/cluster/local/LocalBridgeDataClient.java
+++ opendaylight/networkconfiguration/neutron/midonet/cluster/local/src/main/java/org/opendaylight/controller/networkconfig/neutron/midonet/cluster/local/LocalBridgeDataClient.java
@@ -6,12 +6,16 @@ package org.opendaylight.controller.networkconfig.neutron.midonet.cluster.local;
 
 import java.util.UUID;
 
+import org.midonet.cluster.DataClient;
 import org.midonet.cluster.data.Bridge;
 import org.opendaylight.controller.networkconfig.neutron.midonet.cluster.BridgeDataClient;
 import org.opendaylight.controller.networkconfig.neutron.midonet.cluster.StateAccessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class LocalBridgeDataClient implements BridgeDataClient {
+    private static final Logger logger = LoggerFactory.getLogger(LocalBridgeDataClient.class);
 
     public boolean bridgeExists(UUID id) throws StateAccessException {
         return false;
@@ -25,11 +29,20 @@ public class LocalBridgeDataClient implements BridgeDataClient {
     }
 
     public UUID bridgesCreate(Bridge bridge) throws StateAccessException {
+        UUID bridgeUuid = null;
         if (bridge != null) {
-            bridge.setId(UUID.randomUUID());
+            // TODO(tomohiko) Retrieve a data client instance.
+            DataClient dataClient = null;
+            try {
+                dataClient.bridgesCreate(bridge);
+                bridgeUuid = bridge.getId();
+            } catch (Exception e) {
+                logger.warn("Failed to create a bridge.");
+            }
+            // bridge.setId(UUID.randomUUID());
             return bridge.getId();
         }
-        return null;
+        return bridgeUuid;
     }
 
     public Bridge bridgesGetByName(String tenantId, String name)
